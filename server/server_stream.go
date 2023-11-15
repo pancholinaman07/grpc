811c011ea666f6c414b3deb8061d6ad3a87c5dce
Naman Pancholi <mahendra.naman07@gmail.com>
2023-11-15 15:55:18 +0530

    done

--- server/server_stream.go
+++ server/server_stream.go
@@ -6,13 +6,16 @@ import (
 	"time"
 )
 
-func (s *helloServer) SayHelloServerStream(req *pb.NamesList, stream pb.GreetService_SayHelloServerStreamingServer) error {
-	log.Printf("got request with names : %v", req.Name)
-	for _, name := range req.Name {
-		res := &pb.HelloResponse{Message: "Hello" + name}
+func (s *helloServer) SayHelloServerStreaming(req *pb.NamesList, stream pb.GreetService_SayHelloServerStreamingServer) error {
+	log.Printf("Got request with names : %v", req.Names)
+	for _, name := range req.Names {
+		res := &pb.HelloResponse{
+			Message: "Hello " + name,
+		}
 		if err := stream.Send(res); err != nil {
 			return err
 		}
+		// 2 second delay to simulate a long running process
 		time.Sleep(2 * time.Second)
 	}
 	return nil
